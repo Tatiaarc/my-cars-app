92d3eaac86adb20a450e2387bb54ec18ae829b02
Tatiana <tatianaarc492@gmail.com>
2022-10-10 23:51:52 -0500

    Change axios

--- src/api/cars.api.js
+++ src/api/cars.api.js
@@ -1,16 +1,23 @@
 import axios from "axios";
 
 export const getCarsRequest = async () =>
-  await axios.get("http://localhost:4000/cars");
+  await axios.get("https://my-cars-app-production.up.railway.app/cars");
 
 export const createCarRequest = async (car) =>
-  await axios.post("http://localhost:4000/cars", car);
+  await axios.post("https://my-cars-app-production.up.railway.app/cars", car);
 
 export const deleteCarRequest = async (car_id) =>
-  await axios.delete(`http://localhost:4000/cars/${car_id}`);
+  await axios.delete(
+    `https://my-cars-app-production.up.railway.app/cars/${car_id}`
+  );
 
 export const getCarRequest = async (car_id) =>
-  await axios.get(`http://localhost:4000/cars/${car_id}`);
+  await axios.get(
+    `https://my-cars-app-production.up.railway.app/cars/${car_id}`
+  );
 
 export const updateCarRequest = async (car_id, newFields) =>
-  axios.put(`http://localhost:4000/cars/${car_id}`, newFields);
+  axios.put(
+    `https://my-cars-app-production.up.railway.app/cars/${car_id}`,
+    newFields
+  );
