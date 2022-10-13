cc882e34ecf2f53d9023b432c740f4bb05380369
Tatiana <tatianaarc492@gmail.com>
2022-10-13 00:21:29 -0500

    Cors provitional

--- server/index.js
+++ server/index.js
@@ -6,15 +6,15 @@ import carRoutes from "./routes/cars.routes.js";
 
 const app = express();
 
-const corsOptions = {
-  origin: "https://my-cars-app-production-6336.up.railway.app",
-  optionsSuccessStatus: 200,
-};
+// const corsOptions = {
+//   origin: "https://my-cars-app-production-6336.up.railway.app",
+//   optionsSuccessStatus: 200,
+// };
 
-// app.use(cors());
-app.get("/cars/:id", cors(corsOptions), function (req, res, next) {
-  res.json({ msg: "This is CORS-enabled for only gorgeous_fall." });
-});
+// app.get("/cars/:id", cors(corsOptions), function (req, res, next) {
+//   res.json({ msg: "This is CORS-enabled for only gorgeous_fall." });
+// });
+app.use(cors());
 app.use(express.json());
 
 app.use(indexRoutes);
